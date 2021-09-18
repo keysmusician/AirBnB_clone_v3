01eb4f3b27ceafed7d500ff934788f6102f23e79
keysmusician <justin.masayda@gmail.com>
2021-09-18 15:42:00 +0000

    Fix usage of storage.count()

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
@@ -15,12 +21,12 @@ def status_check():
 def class_total():
     """Retrieves the number of each object by type"""
     class_counts = {
-        "amenities": storage.count("Amenities"),
-        "cities": storage.count("Cities"),
-        "places": storage.count("Places"),
-        "reviews": storage.count("Reviews"),
-        "states": storage.count("States"),
-        "users": storage.count("Users")
+        "amenities": storage.count(Amenity),
+        "cities":    storage.count(City),
+        "places":    storage.count(Place),
+        "reviews":   storage.count(Review),
+        "states":    storage.count(State),
+        "users":     storage.count(User)
     }
 
     return jsonify(class_counts)
