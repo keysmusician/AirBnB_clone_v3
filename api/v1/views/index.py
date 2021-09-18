9a64ee0e5c4b15c11b9bc389e097bcf9974bbe51
keysmusician <justin.masayda@gmail.com>
2021-09-18 19:46:30 +0000

    Add task 7

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Creates a route that returns a JSON"""
+"""Creates index routes"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
---
+++ api/v1/views/states.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Creates index routes"""
+from flask.globals import request
+from sqlalchemy.sql.expression import update
+from api.v1.views import app_views
+from flask import abort, jsonify
+from models import storage
+from models.state import State
+
+
+@app_views.route('/states/', methods=['GET', 'POST'])
+def get_states():
+    """Returns a JSON list of all states"""
+    if request.method == 'GET':
+        return jsonify(
+            [state.to_dict() for state in storage.all(State).values()]
+        )
+    if request.method == 'POST':
+        post = request.get_json()
+        if post is None:
+            return ('Not a JSON', 400)
+        name = post.get('name')
+        if name is None:
+            return ('Missing name', 400)
+        new_state = State(name=name)
+        storage.new(new_state)
+        return (jsonify(new_state.to_dict()), 201)
+
+
+@app_views.route('/states/<id>/', methods=['DELETE', 'GET', 'PUT'])
+def get_state_from_id(id):
+    """Returns a JSONified State specified by ID"""
+    state = storage.get(State, id)
+    if state is None:
+        abort(404)
+
+    if request.method == 'GET':
+        return jsonify(state.to_dict())
+    elif request.method == 'DELETE':
+        state.delete()
+        return ('{}', 200)
+    elif request.method == 'PUT':
+        # Get the update values
+        post = request.get_json()
+        if post is None:
+            return ('Not a JSON', 400)
+        # Ignore these reserved keys
+        for key in ('id', 'created_at', 'updated_at'):
+            post.pop(key, None)
+
+        updated_State = State(state.to_dict().update(post))
+        updated_State.save()
+
+        return (jsonify(updated_State.to_dict()), 200)
