3cefcd4cea6a750c0ccc5180f86f4fcafe6a0c5c
Carson Stearn <cstearn@gmail.com>
2021-09-20 14:00:44 +0000

    task 10: users finished

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -26,3 +26,33 @@ def get_users():
         new_user = User(name=name)
         new_user.save()
         return (jsonify(new_user.to_dict()), 201)
+
+
+@app_views.route('/users/<id>', methods=['DELETE', 'GET', 'PUT'],
+                 strict_slashes=False)
+def get_user_from_id(id):
+    """Returns a JSONified User specified by ID"""
+    user = storage.get("User", id)
+    if user is None:
+        abort(404)
+
+    if request.method == 'GET':
+        return jsonify(user.to_dict())
+    elif request.method == 'DELETE':
+        user.delete()
+        return ('{}', 200)
+    elif request.method == 'PUT':
+        # Get the update values
+        post = request.get_json(silent=True)
+        if post is None:
+            return ('Not a JSON', 400)
+
+        # Update the User
+        for key, value in post.items():
+            # Ignore these reserved keys
+            if key in ('id', 'created_at', 'updated_at'):
+                continue
+            setattr(user, key, value)
+        user.save()
+
+        return (jsonify(user.to_dict()), 200)
