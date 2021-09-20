9e1ea77f51a70c98fe4b80528de1bc7b6574fcee
Carson Stearn <cstearn@gmail.com>
2021-09-20 16:53:47 +0000

    adding get count

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,7 +68,7 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-        
+
     def get(self, cls_name, id):
         """
         Returns one model instance specified by class name and id, None if not
